5b405925ec53615946ef5b84dae4a3e19eace164
Alagie Sellu <alagiesellu@gmail.com>
2022-11-07 23:34:49 +0000

    Add documentation.

---
+++ README.md
@@ -0,0 +1,6 @@
+### Course Web Page:
+- https://nkcs.iops.ai/courses/algorithm
+
+### Assignment Submission
+- http://10.134.1.2/nkuoj
+
---
+++ week_9.cpp
@@ -0,0 +1,42 @@
+/** Exercise 5
+ *
+    ■ Maximum Subarray
+
+    ■ Description
+        – Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+        – A subarray is a contiguous part of an array.
+        – If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+    ■ Input
+        – The first line of the input is an integer n, which is the length of array.
+        – Each of the next n lines contains a single integer in the array.
+
+    ■ Output
+        – The largest sum of the contiguous subarray.
+
+    ■ Deadline
+        – 2022-10-11 23:59:00 GMT+8
+
+ * */
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void getInputs(string &inputs) {
+    cin >> inputs;
+}
+
+int main() {
+
+    string input;
+
+    getInputs(input);
+
+    // Here
+
+    cout << endl;
+
+    return 0;
+}
